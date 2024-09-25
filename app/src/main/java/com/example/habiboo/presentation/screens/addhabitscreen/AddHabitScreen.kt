3ba71f8a465e9912a789381cb7adbb4383568ea5
garuda <rrabosapka@gmail.com>
2024-09-25 14:36:39 +0200

    Implement initial layout for AddHabitScreen

--- app/src/main/java/com/example/habiboo/presentation/screens/addhabitscreen/AddHabitScreen.kt
+++ app/src/main/java/com/example/habiboo/presentation/screens/addhabitscreen/AddHabitScreen.kt
@@ -1,28 +1,150 @@
 package com.example.habiboo.presentation.screens.addhabitscreen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.habiboo.presentation.theme.SoftPink80
 
 
 @Composable
-fun AddHabitScreen(navController: NavHostController, vm : AddHabitScreenViewModel = viewModel()) {
+fun AddHabitScreen(navController: NavHostController, vm: AddHabitScreenViewModel = viewModel()) {
     val scrollState = rememberScrollState()
 
     Column(
+        verticalArrangement = Arrangement.spacedBy(15.dp),
         modifier = Modifier
             .fillMaxSize()
+            .padding(horizontal = 15.dp, vertical = 40.dp)
             .verticalScroll(scrollState)
             .background(color = SoftPink80)
     ) {
+        Text(text = "NAME")
+        Text(text = "NOTE...  ....  ....")
+        CheckBoxesBlock(vm)
+    }
+}
 
+@Composable
+fun CheckBoxesBlock(vm: AddHabitScreenViewModel) {
+    val notificationsEnabledChecked by vm.notificationsEnabledChecked
+    val nonDisabledNotificationsChecked by vm.nonDisabledNotificationsChecked
+    val timedNotificationsEnabledChecked by vm.timedNotificationsEnabledChecked
+    val randomNotificationsEnabledChecked by vm.randomNotificationsEnabledChecked
+    val timePeriodNotificationsEnabled by vm.timePeriodNotificationsEnabled
+    val exactTimeNotificationsEnabled by vm.exactTimeNotificationsEnabled
+
+    var textFieldText by remember { mutableStateOf("") }
+
+    Column {
+        SimpleCheckBox(
+            title = "Enable Notifications",
+            initialValue = notificationsEnabledChecked,
+            onCheckedChange = { newValue -> vm.setNotificationsEnabledChecked(newValue) }
+        )
+
+        SimpleCheckBox(
+            title = "Enable Non-Disabled Notifications",
+            initialValue = nonDisabledNotificationsChecked,
+            onCheckedChange = { newValue -> vm.setNonDisabledNotificationsChecked(newValue) },
+            enabled = notificationsEnabledChecked
+        )
+
+        SimpleCheckBox(
+            title = "Enable Random Notifications",
+            initialValue = randomNotificationsEnabledChecked,
+            onCheckedChange = { newValue -> vm.setRandomNotificationsEnabledChecked(newValue) },
+            enabled = notificationsEnabledChecked
+        )
+
+        SimpleCheckBox(
+            title = "Enable Timed Notifications",
+            initialValue = timedNotificationsEnabledChecked,
+            onCheckedChange = { newValue -> vm.setTimedNotificationsEnabledChecked(newValue) },
+            enabled = notificationsEnabledChecked
+        )
+
+        if (timedNotificationsEnabledChecked) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.padding(start = 20.dp)
+            ) {
+                Checkbox(
+                    checked = timePeriodNotificationsEnabled,
+                    onCheckedChange = { vm.setTimePeriodNotificationsEnabled(it)}
+                )
+                Text(
+                    text = "every",
+                )
+
+                TextField(
+                    value = textFieldText,
+                    onValueChange = { newText -> textFieldText = newText },
+                    Modifier.width(35.dp).height(30.dp)
+                )
+                Text(text = "hours.")
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.padding(start = 20.dp)
+            ) {
+                Checkbox(
+                    checked = exactTimeNotificationsEnabled,
+                    onCheckedChange = { vm.setExactTimeNotificationsEnabled(it)}
+                )
+                Text(
+                    text = "at:",
+                )
+                //add time picker and list od dates
+            }
+        }
+    }
+}
+
+@Composable
+fun SimpleCheckBox(
+    title: String,
+    initialValue: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    enabled: Boolean = true
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = initialValue,
+            onCheckedChange = { newCheckedValue ->
+                onCheckedChange(newCheckedValue)
+            },
+            enabled = enabled
+        )
+        Text(
+            text = title,
+            color = if (enabled) Color.Black else Color.Gray,
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
 }
--- app/src/main/java/com/example/habiboo/presentation/screens/addhabitscreen/AddHabitScreenViewModel.kt
+++ app/src/main/java/com/example/habiboo/presentation/screens/addhabitscreen/AddHabitScreenViewModel.kt
@@ -1,8 +1,52 @@
 package com.example.habiboo.presentation.screens.addhabitscreen
 
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class AddHabitScreenViewModel () : ViewModel() {
 
+class AddHabitScreenViewModel : ViewModel() {
+
+    private val _notificationsEnabledChecked = mutableStateOf(false)
+    val notificationsEnabledChecked: State<Boolean> = _notificationsEnabledChecked
+
+    private val _nonDisabledNotificationsChecked = mutableStateOf(false)
+    val nonDisabledNotificationsChecked: State<Boolean> = _nonDisabledNotificationsChecked
+
+    private val _timedNotificationsEnabledChecked = mutableStateOf(false)
+    val timedNotificationsEnabledChecked: State<Boolean> = _timedNotificationsEnabledChecked
+
+    private val _randomNotificationsEnabledChecked = mutableStateOf(false)
+    val randomNotificationsEnabledChecked: State<Boolean> = _randomNotificationsEnabledChecked
+
+    private val _timePeriodNotificationsEnabled = mutableStateOf(false)
+    val timePeriodNotificationsEnabled: State<Boolean> = _timePeriodNotificationsEnabled
+
+    private val _exactTimeNotificationsEnabled = mutableStateOf(false)
+    val exactTimeNotificationsEnabled: State<Boolean> = _exactTimeNotificationsEnabled
+
+    fun setNotificationsEnabledChecked(value: Boolean) {
+        _notificationsEnabledChecked.value = value
+        if (!value) {
+            _nonDisabledNotificationsChecked.value = false
+            _timedNotificationsEnabledChecked.value = false
+            _randomNotificationsEnabledChecked.value = false
+        }
+    }
+
+    fun setNonDisabledNotificationsChecked(value: Boolean) {
+        _nonDisabledNotificationsChecked.value = value
+    }
+    fun setTimedNotificationsEnabledChecked(value: Boolean) {
+        _timedNotificationsEnabledChecked.value = value
+    }
+    fun setRandomNotificationsEnabledChecked(value: Boolean) {
+        _randomNotificationsEnabledChecked.value = value
+    }
+    fun setTimePeriodNotificationsEnabled(value: Boolean) {
+        _timePeriodNotificationsEnabled.value = value
+    }
+    fun setExactTimeNotificationsEnabled(value: Boolean) {
+        _exactTimeNotificationsEnabled.value = value
+    }
 }
