b3043b866493d1926d5775f977c24963323cf926
garuda <rrabosapka@gmail.com>
2024-10-25 13:47:09 +0200

    aboba

--- app/src/main/java/com/example/habiboo/presentation/screens/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/habiboo/presentation/screens/loginscreen/LoginScreen.kt
@@ -1,58 +1,90 @@
 package com.example.habiboo.presentation.screens.loginscreen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import com.example.habiboo.presentation.navigation.NavDestination
-import com.example.habiboo.presentation.screens.homescreen.AddHabitButton
-import com.example.habiboo.presentation.screens.homescreen.EmptyListPlaceHolder
-import com.example.habiboo.presentation.screens.homescreen.HabitList
-import com.example.habiboo.presentation.screens.homescreen.HomeScreenViewModel
-import com.example.habiboo.presentation.screens.homescreen.TopBar
-
+import com.example.habiboo.R
 
 
 @Composable
 fun LoginScreen(navController: NavHostController ){
-    Row {
-      Text(text ="HabiBoo", )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.fire_icon),
+            contentDescription = "Background Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize().padding(32.dp)
+        ) {
+            Text(
+                text = "HabiBoo",
+                fontSize = 34.sp,
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextButton(
+                onClick = { /* Handle sign up */ },
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+            ) {
+                Text("Forgot Password?")
+            }
+
+            Button(
+                onClick = { /* Handle sign in */ },
+                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+            ) {
+                Text("Sign in")
+            }
+            TextButton(
+                onClick = { /* Handle sign up */ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Sign up")
+            }
+        }
     }
 }
 
-@Composable
-fun HomeScreen(navController: NavHostController, vm: HomeScreenViewModel = viewModel()) {
-
-    val habitsState = vm.habits.observeAsState()
-    val onAddBtnClicked  = {  navController.navigate(NavDestination.AddHabit.route)  }
-
-    Scaffold(
-        topBar = { TopBar() },
-        floatingActionButton = { AddHabitButton(onAddBtnClicked = onAddBtnClicked) },
-        content = { padding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding)
-            ) {
-                if (habitsState.value?.isEmpty() == true) {
-                    EmptyListPlaceHolder()
-                } else {
-                    habitsState.value?.let { HabitList(it,
-                        onHabitClick = { habitId ->
-                            //   navController.navigate(NavDestination.A.createRoute(habitId))
-                        })  }
-                }
-            }
-        }
-    )
-
-}
--- app/src/main/java/com/example/habiboo/presentation/theme/Type.kt
+++ app/src/main/java/com/example/habiboo/presentation/theme/Type.kt
@@ -2,33 +2,46 @@ package com.example.habiboo.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.habiboo.R
 
-// Set of Material typography styles to start with
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, weight = FontWeight.Normal),
+    Font(R.font.poppins_bold, weight = FontWeight.Bold),
+    Font(R.font.poppins_black, weight = FontWeight.Black),
+    Font(R.font.poppins_light, weight = FontWeight.Light)
+)
+
+val SulphurPoint = FontFamily(
+    Font(R.font.sulphur_point_regular, weight = FontWeight.Normal),
+    Font(R.font.sulphur_point_bold, weight = FontWeight.Bold),
+    Font(R.font.sulphur_point_light, weight = FontWeight.Light)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = SulphurPoint,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
